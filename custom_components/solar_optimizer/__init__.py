f912fa8d3233267fd099fbfb9d06bf77522b39ee
Jean-Marc Collin <jean-marc.collin-extern@renault.com>
2023-08-16 20:07:02 +0000

    Add CONFIG_SCHEMA

--- custom_components/solar_optimizer/__init__.py
+++ custom_components/solar_optimizer/__init__.py
@@ -1,12 +1,20 @@
 """Initialisation du package de l'intégration HACS Tuto"""
 import logging
+import voluptuous as vol
 
 from homeassistant.const import EVENT_HOMEASSISTANT_START
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import ConfigType
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers import selector
+from homeassistant.components.input_boolean import DOMAIN as INPUT_BOOLEAN_DOMAIN
+from homeassistant.components.input_number import DOMAIN as INPUT_NUMBER_DOMAIN
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 
 # from homeassistant.helpers.entity_component import EntityComponent
+
 
 from .const import DOMAIN, PLATFORMS
 from .coordinator import SolarOptimizerCoordinator
@@ -14,6 +22,58 @@ from .coordinator import SolarOptimizerCoordinator
 # from .input_boolean import async_setup_entry as async_setup_entry_input_boolean
 
 _LOGGER = logging.getLogger(__name__)
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                "algorithm": vol.Schema(
+                    {
+                        vol.Required("initial_temp", default=1000): vol.Coerce(float),
+                        vol.Required("min_temp", default=0.1): vol.Coerce(float),
+                        vol.Required("cooling_factor", default=0.95): vol.Coerce(float),
+                        vol.Required(
+                            "max_iteration_number", default=1000
+                        ): cv.positive_int,
+                    }
+                ),
+                "devices": vol.All(
+                    [
+                        {
+                            vol.Required("name"): str,
+                            vol.Required("entity_id"): selector.EntitySelector(
+                                selector.EntitySelectorConfig(
+                                    domain=[INPUT_BOOLEAN_DOMAIN, SWITCH_DOMAIN]
+                                )
+                            ),
+                            vol.Optional("power_entity_id"): selector.EntitySelector(
+                                selector.EntitySelectorConfig(
+                                    domain=[INPUT_NUMBER_DOMAIN, SENSOR_DOMAIN]
+                                )
+                            ),
+                            vol.Required("power_max"): vol.Coerce(float),
+                            vol.Optional("power_min"): vol.Coerce(float),
+                            vol.Optional("power_step"): vol.Coerce(float),
+                            vol.Optional("check_usable_template"): str,
+                            vol.Optional("check_active_template"): str,
+                            vol.Optional("duration_min"): vol.Coerce(float),
+                            vol.Optional("duration_stop_min"): vol.Coerce(float),
+                            vol.Optional("duration_power_min"): vol.Coerce(float),
+                            vol.Optional("action_mode"): str,
+                            vol.Required("activation_service"): str,
+                            vol.Required("deactivation_service"): str,
+                            vol.Optional("change_power_service"): str,
+                            vol.Optional("convert_power_divide_factor"): vol.Coerce(
+                                float
+                            ),
+                        }
+                    ]
+                ),
+            }
+        ),
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
 
 async def async_setup(
