9f57ee440999586ce2b434787f7ae61ce7f33549
Jean-Marc Collin <jean-marc.collin-extern@renault.com>
2025-03-09 17:40:45 +0000

    With simple and powered device tests ok

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -4,6 +4,7 @@
 
 import itertools
 import pytest
+from datetime import time
 
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType, InvalidData
@@ -201,3 +202,143 @@ async def test_wrong_raz_time(
     assert _result["step_id"] == "device_central"
     assert _result["type"] == FlowResultType.FORM
     assert _result["errors"] == {"raz_time": "format_time_invalid"}
+
+
+async def test_simple_device_config(
+    hass: HomeAssistant, skip_hass_states_get, init_solar_optimizer_central_config
+):
+    """Test a simple device configuration"""
+    result = await hass.config_entries.flow.async_init(
+        config_flow.DOMAIN, context={"source": "user"}
+    )
+
+    assert result["step_id"] == "user"
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {}
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={CONF_DEVICE_TYPE: CONF_DEVICE}
+    )
+
+    assert result["step_id"] == "device"
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {}
+
+    user_input = {
+        CONF_NAME: "Equipement A",
+        CONF_ENTITY_ID: "input_boolean.fake_device_a",
+        CONF_POWER_MAX: 1000,
+        CONF_CHECK_USABLE_TEMPLATE: "{{ True }}",
+        CONF_CHECK_ACTIVE_TEMPLATE: "{{ False }}",
+        CONF_DURATION_MIN: 0.3,
+        CONF_DURATION_STOP_MIN: 0.1,
+        CONF_ACTION_MODE: CONF_ACTION_MODE_ACTION,
+        CONF_ACTIVATION_SERVICE: "input_boolean/turn_on",
+        CONF_DEACTIVATION_SERVICE: "input_boolean/turn_off",
+        CONF_BATTERY_SOC_THRESHOLD: 50,
+        CONF_MAX_ON_TIME_PER_DAY_MIN: 10,
+        CONF_MIN_ON_TIME_PER_DAY_MIN: 1,
+        CONF_OFFPEAK_TIME: "22:00",
+    }
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+    data = result.get("data")
+    assert data is not None
+
+    assert data.get(CONF_NAME) == "Equipement A"
+    assert data.get(CONF_POWER_MAX) == 1000
+    assert data.get(CONF_POWER_MIN) is None
+    assert data.get(CONF_POWER_STEP) is None
+    assert data.get(CONF_CHECK_USABLE_TEMPLATE) == "{{ True }}"
+    assert data.get(CONF_CHECK_ACTIVE_TEMPLATE) == "{{ False }}"
+    assert data.get(CONF_DURATION_MIN) == 0.3
+    assert data.get(CONF_DURATION_STOP_MIN) == 0.1
+    assert data.get(CONF_DURATION_POWER_MIN) is None
+    assert data.get(CONF_ACTION_MODE) == CONF_ACTION_MODE_ACTION
+    assert data.get(CONF_ACTIVATION_SERVICE) == "input_boolean/turn_on"
+    assert data.get(CONF_DEACTIVATION_SERVICE) == "input_boolean/turn_off"
+    assert data.get(CONF_CONVERT_POWER_DIVIDE_FACTOR) is None
+    assert data.get(CONF_CHANGE_POWER_SERVICE) is None
+    assert data.get(CONF_POWER_ENTITY_ID) is None
+    assert data.get(CONF_BATTERY_SOC_THRESHOLD) == 50
+    assert data.get(CONF_MAX_ON_TIME_PER_DAY_MIN) == 10
+    assert data.get(CONF_MIN_ON_TIME_PER_DAY_MIN) == 1
+    assert data.get(CONF_OFFPEAK_TIME) == "22:00"
+
+
+async def test_powered_device_config(
+    hass: HomeAssistant, skip_hass_states_get, init_solar_optimizer_central_config
+):
+    """Test a simple device configuration"""
+    result = await hass.config_entries.flow.async_init(
+        config_flow.DOMAIN, context={"source": "user"}
+    )
+
+    assert result["step_id"] == "user"
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {}
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={CONF_DEVICE_TYPE: CONF_POWERED_DEVICE}
+    )
+
+    assert result["step_id"] == "powered_device"
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {}
+
+    user_input = {
+        CONF_NAME: "Equipement A",
+        CONF_ENTITY_ID: "input_boolean.fake_device_a",
+        CONF_POWER_MAX: 1000,
+        CONF_POWER_MIN: 100,
+        CONF_POWER_STEP: 150,
+        CONF_CHECK_USABLE_TEMPLATE: "{{ True }}",
+        CONF_CHECK_ACTIVE_TEMPLATE: "{{ False }}",
+        CONF_DURATION_MIN: 0.3,
+        CONF_DURATION_STOP_MIN: 0.1,
+        CONF_DURATION_POWER_MIN: 3,
+        CONF_ACTION_MODE: CONF_ACTION_MODE_ACTION,
+        CONF_ACTIVATION_SERVICE: "input_boolean/turn_on",
+        CONF_DEACTIVATION_SERVICE: "input_boolean/turn_off",
+        CONF_CONVERT_POWER_DIVIDE_FACTOR: 6,
+        CONF_CHANGE_POWER_SERVICE: "input_number/set_value",
+        CONF_POWER_ENTITY_ID: "input_number.tesla_amps",
+        CONF_BATTERY_SOC_THRESHOLD: 50,
+        CONF_MAX_ON_TIME_PER_DAY_MIN: 10,
+        CONF_MIN_ON_TIME_PER_DAY_MIN: 1,
+        CONF_OFFPEAK_TIME: "22:00",
+    }
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+    data = result.get("data")
+    assert data is not None
+
+    assert data.get(CONF_NAME) == "Equipement A"
+    assert data.get(CONF_POWER_MAX) == 1000
+    assert data.get(CONF_POWER_MIN) == 100
+    assert data.get(CONF_POWER_STEP) == 150
+    assert data.get(CONF_CHECK_USABLE_TEMPLATE) == "{{ True }}"
+    assert data.get(CONF_CHECK_ACTIVE_TEMPLATE) == "{{ False }}"
+    assert data.get(CONF_DURATION_MIN) == 0.3
+    assert data.get(CONF_DURATION_STOP_MIN) == 0.1
+    assert data.get(CONF_DURATION_POWER_MIN) == 3
+    assert data.get(CONF_ACTION_MODE) == CONF_ACTION_MODE_ACTION
+    assert data.get(CONF_ACTIVATION_SERVICE) == "input_boolean/turn_on"
+    assert data.get(CONF_DEACTIVATION_SERVICE) == "input_boolean/turn_off"
+    assert data.get(CONF_CONVERT_POWER_DIVIDE_FACTOR) == 6
+    assert data.get(CONF_CHANGE_POWER_SERVICE) == "input_number/set_value"
+    assert data.get(CONF_POWER_ENTITY_ID) == "input_number.tesla_amps"
+    assert data.get(CONF_BATTERY_SOC_THRESHOLD) == 50
+    assert data.get(CONF_MAX_ON_TIME_PER_DAY_MIN) == 10
+    assert data.get(CONF_MIN_ON_TIME_PER_DAY_MIN) == 1
+    assert data.get(CONF_OFFPEAK_TIME) == "22:00"
