c8c310f069f4c68674d34491aeae55254f6b569f
Jean-Marc Collin <jean-marc.collin-extern@renault.com>
2025-05-25 09:45:06 +0000

    Fix device forced during off-peak hours when the minimum time has been reached during the day #140

--- tests/test_min_on_time.py
+++ tests/test_min_on_time.py
@@ -448,3 +448,98 @@ async def test_nominal_use_offpeak_without_min(
         now = datetime(2024, 11, 10, 1, 00, 00).replace(tzinfo=get_tz(hass))
         device_a._set_now(now)
         assert device_a.should_be_forced_offpeak is False
+
+
+async def test_nominal_use_min_on_time_exceeded(hass: HomeAssistant, init_solar_optimizer_central_config):
+    """Testing the nominal case with min_on_time and offpeak_time"""
+
+    entry_a = MockConfigEntry(
+        domain=DOMAIN,
+        title="Equipement A",
+        unique_id="eqtAUniqueId",
+        data={
+            CONF_NAME: "Equipement A",
+            CONF_DEVICE_TYPE: CONF_DEVICE,
+            CONF_ENTITY_ID: "input_boolean.fake_device_a",
+            CONF_POWER_MAX: 1000,
+            CONF_CHECK_USABLE_TEMPLATE: "{{ True }}",
+            CONF_DURATION_MIN: 2,
+            CONF_DURATION_STOP_MIN: 1,
+            CONF_ACTION_MODE: CONF_ACTION_MODE_ACTION,
+            CONF_ACTIVATION_SERVICE: "input_boolean/turn_on",
+            CONF_DEACTIVATION_SERVICE: "input_boolean/turn_off",
+            CONF_BATTERY_SOC_THRESHOLD: 30,
+            CONF_MAX_ON_TIME_PER_DAY_MIN: 10,
+            CONF_MIN_ON_TIME_PER_DAY_MIN: 2,
+            CONF_OFFPEAK_TIME: "23:00",
+        },
+    )
+
+    device = await create_managed_device(
+        hass,
+        entry_a,
+        "equipement_a",
+    )
+    assert device is not None
+
+    assert device.name == "Equipement A"
+
+    coordinator: SolarOptimizerCoordinator = SolarOptimizerCoordinator.get_coordinator()
+    # default value
+    assert coordinator.raz_time == time(5, 0, 0)
+
+    device_a: ManagedDevice = coordinator.devices[0]
+    assert device_a.min_on_time_per_day_sec == 2 * 60
+    assert device_a.offpeak_time == time(23, 0)
+
+    now = datetime(2024, 11, 10, 10, 00, 00).replace(tzinfo=get_tz(hass))
+    device_a._set_now(now)
+
+    # Creates the fake input_boolean (the device)
+    await create_test_input_boolean(hass, device_a.entity_id, "fake underlying A")
+    fake_input_bool = search_entity(hass, "input_boolean.fake_device_a", INPUT_BOOLEAN_DOMAIN)
+    assert fake_input_bool is not None
+
+    # The on_time should be 0
+    device_a_on_time_sensor = search_entity(hass, "sensor.on_time_today_solar_optimizer_equipement_a", SENSOR_DOMAIN)
+    assert device_a_on_time_sensor.state == 0
+    assert device_a_on_time_sensor.last_datetime_on is None
+    device_a.reset_next_date_available("Activate")  # for test only to reset the next_available date
+
+    #
+    # 1. Activate the underlying switch
+    #
+    assert device_a.is_usable is False  # because duration_min is 3 minutes
+    assert device_a.is_enabled is True
+
+    await fake_input_bool.async_turn_on()
+    await hass.async_block_till_done()
+
+    #
+    # 2. stop the unerlying switch, after 3 minutes.
+    #
+    now = now + timedelta(minutes=4)
+    device_a._set_now(now)
+
+    assert device_a.is_usable is True
+    # before 23:00 device should not be forcable
+    assert device_a.should_be_forced_offpeak is False
+
+    await fake_input_bool.async_turn_off()
+    await hass.async_block_till_done()
+
+    assert device_a_on_time_sensor.state == 4 * 60
+
+    #
+    # 3. before 23:00, the device should not be Usable
+    #
+    now = datetime(2024, 11, 10, 22, 59, 59).replace(tzinfo=get_tz(hass))
+    device_a._set_now(now)
+    assert device_a.should_be_forced_offpeak is False
+
+    #
+    # 4. just after 23:00 it should be possible to force offpeak
+    #
+    now = datetime(2024, 11, 10, 23, 00, 00).replace(tzinfo=get_tz(hass))
+    device_a._set_now(now)
+    assert device_a.should_be_forced_offpeak is True
